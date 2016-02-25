58b8e64aa2d28de733d902c120dd28f25be17a8c
Gergo Horanyi <gergo.horanyi@prezi.com>
2016-02-25 03:39:57 +0100

    Adding hostname to footer

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import socket
 
+hostname = socket.gethostname()
 
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'hostname': hostname})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'hostname': hostname})
