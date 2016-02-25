ee60eb2e899d51ac3185bfcff6ca7281c0cbb150
Gergo Horanyi <gergo.horanyi@prezi.com>
2016-02-25 13:41:26 +0100

    Changing database

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,23 +56,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'budapestpy',
-#         'USER': 'root',
-#         'PASSWORD': 'budapestpy',
-#         'HOST': 'budapestpy.cbmcttzahuz7.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'budapestpy',
+        'USER': 'root',
+        'PASSWORD': 'budapestpy',
+        'HOST': 'budapestpy.cbmcttzahuz7.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+# 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
